950a72955e401785b6ab05ef33bb83a6f22b36d7
CarlosAndresQuel00 <66259796+CarlosAndresQuel00@users.noreply.github.com>
2020-07-06 11:54:24 -0500

    Add files via upload

--- MixedCase.java
+++ MixedCase.java
@@ -1,36 +1,27 @@
 import java.util.Scanner;
 
-/**
-Obtener una cadena del usuario y almacenarla en:
-String s;
+public class MixedCase {
+    public static void main(String[] args) {
 
-Escribe un bucle for que imprima la cadena alternando las mayusulas y minusculas, siendo la primera letra siempre minuscula.
-
-Ten en cuenta los siguientes metodos de la clase String:
-str.toUpperCase(); // make it uppercase
-str.toLowerCase(); // make it lowercase 
-
-NO UTILICES EL METODO .chatAt ya que este retorna un char y lo que necesitas es un String para hacerlo mayuscula o minuscula. Para obtener una letra en una posicion,
-puedes usar el siguiente metodo:
-str.substring(x, x+1); // devuelve el caracter como string en la posicion x
-
-Ejemplos de entrada/salida:
-In: powerful
-pOwErFuL
-
-In: COMEDIC
-cOmEdIc
-
-In: acroBATics
-aCrObAtIcS
-
-PISTA: Necesitaras una sentencia if dentro del for! 
-*/
-class MixedCase {
-  public static void main(String[] args) {
     Scanner inp = new Scanner(System.in);
-    System.out.print("In:");
+
+    System.out.print("In: ");
+
     String s = inp.nextLine();
-    //write your code below
+    String cad1 = "";
+    String cad;
+
+    for (int i = 0; i < s.length(); i++) {
+        cad = s.substring(i, i+1);
+        if (i == 0) {
+            cad1 = cad.toLowerCase();
+        } else if (i % 2 == 0) {
+            cad1 += cad.toLowerCase();
+        } else {
+            cad1 += cad.toUpperCase();
+        }
+    }
+        System.out.println(cad1);
+
   }
 }
