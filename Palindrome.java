daf8b7d063463c48ae0a40129570842b96f4a812
CarlosAndresQuel00 <66259796+CarlosAndresQuel00@users.noreply.github.com>
2020-07-06 12:02:44 -0500

    Add files via upload

--- Palindrome.java
+++ Palindrome.java
@@ -1,31 +1,38 @@
 import java.util.Scanner;
 
-/**
-Escribe un programa que pida una cadena al usuario y que revise si esta es un palindromo o no y terminar.
+public class Palindrome {
+    public static void main(String[] args) {
 
-Un palindromo significa que los caracteres de la cadena son los mismos al leer hacia adelante o hace atras.
-Debes ignorar los espacios, los acentos, puntuaciones y mayusculas y minusculas.
-
-Ejemplos de palindromos:
-Amor a Roma
-Bob
-Somos o no somos
-Acaso hubo buhos aca.
-Ana, la tacana catalana
-racecar
-was it a car or a cat I saw
-never odd or even
-rats live on no evil star
-
-El programa debe imprimir true si es un palindromo y false si no lo es.
-*/
-class Palindrome {
-  public static void main(String[] args) {
     Scanner inp = new Scanner(System.in);
+    
+    boolean ok = false;
+
     System.out.print("In:");
+
     String s = inp.nextLine();
-   	//write your code below
-   	
-   	
+
+   	char[] letter = s.toCharArray();
+        
+        if (letter.length % 2 == 0) {
+            for (int i = 0; i < letter.length/2-1; i++) {
+                if (letter[i] != letter[letter.length-i-1]) {
+                    ok = false;
+                } else {
+                    ok = true;
+                }
+            }
+        } else {
+            for (int i = 0; i < (letter.length-1)/2-1; i++) {
+                if (letter[i] != letter[letter.length-i-1]) {
+                    ok = false;
+                } else {
+                    ok = true;
+                }
+            }
+        }
+        
+        System.out.println(ok);
+        
+
   }
 }
